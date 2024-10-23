2091822f837cb0b0a0260b1b4411909ec8ec1016
Ankith G <ankithg03@gmail.com>
2024-10-23 14:29:20 +0530

    Navbar UI updated

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,21 +4,23 @@ import Link from 'next/link';
 
 export default function Navigation() {
   return (
-    <nav className="flex justify-between items-center p-4 bg-gray-100">
-      <Link href="/" className="flex items-center space-x-2">
-        <svg
-          viewBox="0 0 24 24"
-          className="h-6 w-6 fill-current"
-          style={{ fill: '#6200EA' }}
-        >
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-        </svg>
-        <span className="font-bold text-xl" style={{ color: '#6200EA' }}>
-          {"Ank's Splitwise"}
-        </span>
-      </Link>
-
-      <div className="flex items-center space-x-12">
+    <nav className="w-full flex flex-col justify-between items-center p-4 bg-gray-100">
+     <div className='flex w-full justify-between'>
+          <Link href="/" className="flex items-center space-x-2">
+            <svg
+              viewBox="0 0 24 24"
+              className="h-6 w-6 fill-current"
+              style={{ fill: '#6200EA' }}
+            >
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+            </svg>
+            <span className="font-bold text-xl" style={{ color: '#6200EA' }}>
+              {"Ank's Splitwise"}
+            </span>
+          </Link>
+          <UserButton />
+     </div>
+      <div className="grid w-full md:flex items-center md:space-x-2 mt-2">
         {/* Added new link for 'Your Groups' */}
         <Link
           href="/groups"
@@ -38,7 +40,6 @@ export default function Navigation() {
         >
           Add Expense
         </Link>
-        <UserButton />
       </div>
     </nav>
   );
